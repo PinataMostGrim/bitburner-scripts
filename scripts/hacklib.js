084bcc5d62a7c6a50ebf4a22c67bd1ef49715128
Aaron <pinatamostgrim@gmail.com>
2021-12-21 14:52:04 -0400

    Add method to 'hacklib' for determining if root access can be gained

--- scripts/hacklib.js
+++ scripts/hacklib.js
@@ -1,4 +1,27 @@
 /** @param {NS} ns **/
+
+/// <summary>
+/// Returns whether or not root access can be gained on the target host.
+/// </summary>
+export function canGetRootAccess(ns, host)
+{
+    let requiredHackLevel = ns.getServerRequiredHackingLevel(host);
+    let hackLevel = ns.getHackingLevel();
+    if (requiredHackLevel > hackLevel)
+    {
+        return false;
+    }
+
+    let requiredPorts = ns.getServerNumPortsRequired(host);
+    let portsOpenable = getPortsOpenable(ns);
+    if (requiredPorts > portsOpenable)
+    {
+        return false;
+    }
+
+    return true;
+}
+
 /// <summary>
 /// Returns the maximum number of ports that can currently be opened.
 /// </summary>
