a7b9883feaf151528958bd95a5d67e5f48fbedb3
Aaron <pinatamostgrim@gmail.com>
2021-12-21 23:35:40 -0400

    Refactor 'deploy.js' to use 'hacklib'

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,33 @@
 /** @param {NS} ns **/
-export async function main(ns) 
+import {deployScriptOnServer} from "hacklib.js";
+
+export async function main(ns)
 {
     const args = ns.flags([["help", false]]);
-    if (args.help || args._.length < 2) 
+    if (args.help || args._.length < 2)
     {
         ns.tprint("This script deploys another script on a server with maximum threads possible.");
-        ns.tprint(`Usage: run ${ns.getScriptName()} HOST SCRIPT ARGUMENTS`);
+        ns.tprint(`Usage: run ${ns.getScriptName()} SERVER SCRIPT ARGUMENTS`);
         ns.tprint("Example:");
         ns.tprint(`> run ${ns.getScriptName()} n00dles basic_hack.js foodnstuff`);
         return;
     }
 
-    const host = args._[0];
+    const server = args._[0];
     const script = args._[1];
-    const script_args = args._.slice(2);
+    const scriptArgs = args._.slice(2);
 
-    if (!ns.serverExists(host))
+    if (!ns.serverExists(server))
     {
-        ns.tprint(`Server '${host}' does not exist. Aborting.`);
+        ns.tprint(`Server '${server}' does not exist. Aborting.`);
         return;
     }
-    if (!ns.ls(ns.getHostname()).find(f => f === script)) 
+
+    if (!ns.ls(ns.getHostname()).find(f => f === script))
     {
         ns.tprint(`Script '${script}' does not exist. Aborting.`);
         return;
     }
 
-    const threads = Math.floor((ns.getServerMaxRam(host) - ns.getServerUsedRam(host)) / ns.getScriptRam(script));
-    ns.tprint(`Launching script '${script}' on server '${host}' with ${threads} threads and the following arguments: ${script_args}`);
-    await ns.scp(script, ns.getHostname(), host);
-    ns.exec(script, host, threads, ...script_args);
+    await deployScriptOnServer(ns, server, script, ...scriptArgs);
 }
